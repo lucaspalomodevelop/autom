f5e5eb9098e61d0db7632a4edee3ad27e528473c
lucaspalomodevelop <lucas.palomo@t-online.de>
2023-03-07 20:20:01 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # autom
+
+__IN DEVELOPMENT, NOT READY FOR USE__
+
+autom is a command line tool for executing shell scripts, independent of the working directory
---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+
+    // make dir in /etc/autom
+    std::string home = getenv("HOME");
+    std::string dir = home + "/.autom";
+    // std::cout << "dir: " << dir << std::endl;
+    mkdir(dir.c_str(), 0777);
+
+    if (argc > 1 && argv[1][0] != '-')
+    {
+        std::string pre_script = "cd " + dir + " && ";
+        std::string script = pre_script + dir + "/" + argv[1] + ".sh";
+        system(script.c_str());
+    }
+    // else{
+    //     std::cout << "Usage: autom <script>" << std::endl;
+    // }
+
+    return 0;
+}
