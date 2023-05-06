459132eb8e14b7decf0a4caff87a24865068e256
lucaspalomodevelop <lucas.palomo@t-online.de>
2023-05-06 23:31:58 +0200

    added ls to help output

--- src/main.cpp
+++ src/main.cpp
@@ -55,5 +55,6 @@ void help(int argc, char *argv[])
     std::cout << "Commands:" << std::endl;
     std::cout << "  [script] - Runs a script if autom has not command with that name" << std::endl;
     std::cout << "  run [script] - Runs a script" << std::endl;
+    std::cout << "  ls - Lists all scripts" << std::endl;
     std::cout << "  help - Shows this help message" << std::endl;
 }
